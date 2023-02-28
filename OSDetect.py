1db754243fc73513de49f99545bdddb8ab8cd1c0
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-03-01 02:15:03 +0530

    OS Detection Exception

--- OSDetect.py
+++ OSDetect.py
@@ -4,15 +4,17 @@ import socket
 import os
 import subprocess
 print('\n-----------Remote OS Detection Started-----------\n')
-with open('Result_current.txt') as f:
-    hostname = f.readline().strip()
-    print("Detecting Host Operating System for: "+hostname)
-hostip = socket.gethostbyname(hostname)
-nm = nmap.PortScanner()
-machine = nm.scan(hostip, arguments='-O')
-
-print("OS Type: ", machine['scan'][str(hostip)]['osmatch'][0]['osclass'][0]['osfamily'])
-print("Detection Accuracy: ", machine['scan'][str(hostip)]['osmatch'][0]['osclass'][0]['accuracy'])
+try:
+    with open('Result_current.txt') as f:
+        hostname = f.readline().strip()
+        print("Detecting Host Operating System for: "+hostname)
+    hostip = socket.gethostbyname(hostname)
+    nm = nmap.PortScanner()
+    machine = nm.scan(hostip, arguments='-O')
+    print("OS Type: ", machine['scan'][str(hostip)]['osmatch'][0]['osclass'][0]['osfamily'])
+    print("Detection Accuracy: ", machine['scan'][str(hostip)]['osmatch'][0]['osclass'][0]['accuracy'])
+except IndexError:
+    print("Cannot Detect OS for: "+hostname)
 print('\n----Scanning Finished----\n')
 rawpath = os.getcwd() + "\\SSLCertificate.py"
 path = rawpath.replace('\\', '/')
