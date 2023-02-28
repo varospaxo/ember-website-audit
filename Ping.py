b6a53e60d813258bcd996f382a075feac3740bb9
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-03-01 02:49:45 +0530

    Exception Handling

--- Ping.py
+++ Ping.py
@@ -5,20 +5,24 @@ import subprocess
 
 hostname = ""
 print("\n-----------Host Service Detection-----------\n")
-with open('Result_current.txt') as f:
-    hostname = f.readline()
-    print ("Host status for: "+ hostname)
-    ip_address = socket.gethostbyname(hostname.strip())
-    file = open("Result_current.txt", "a")         
-    file.write("\n"+ip_address)
-    file.close()
-print("IP Address: ", ip_address)
-response_list = ping(ip_address, size=40, count=10)
-if response_list.rtt_avg_ms > 0:
-    print ("Host is UP!")
-else:
-    print("Host is DOWN!")
-print("Ping Avg Response Time:", response_list.rtt_avg_ms)
+try:
+    with open('Result_current.txt') as f:
+        hostname = f.readline()
+        print ("Host status for: "+ hostname)
+        ip_address = socket.gethostbyname(hostname.strip())
+        file = open("Result_current.txt", "a")         
+        file.write("\n"+ip_address)
+        file.close()
+    print("IP Address: ", ip_address)
+    response_list = ping(ip_address, size=40, count=10)
+    if response_list.rtt_avg_ms > 0:
+        print ("Host is UP!")
+    else:
+        print("Host is DOWN!")
+    print("Ping Avg Response Time:", response_list.rtt_avg_ms)
+except socket.gaierror:
+    print("Hostname unavailable!")
+    exit()
 print("\n----Scanning Finished----\n")
 rawpath = os.getcwd() + "\\PortScannerFast.py"
 path = rawpath.replace('\\', '/')
--- ReverseDNS.py
+++ ReverseDNS.py
@@ -2,18 +2,21 @@ import socket
 import os
 import subprocess
 print("\n-----------Reverse DNS Scanner Started-----------\n")
-with open('Result_current.txt') as f:
-    hostname = f.readline().strip()
-    currenttime = f.readline().strip()
-    hostip = f.readline().strip()
-    
-    print ("Domain Name: "+hostname)
-    print ("IP Address: "+hostip)
-def get_domain_name(ip_address):
-  result=socket.gethostbyaddr(ip_address)
-  return list(result)[0]
-# print("Domain name using PTR DNS:")
-print("Reverse DNS address: "+get_domain_name(hostip))
+try:
+  with open('Result_current.txt') as f:
+      hostname = f.readline().strip()
+      currenttime = f.readline().strip()
+      hostip = f.readline().strip()
+      
+      print ("Domain Name: "+hostname)
+      print ("IP Address: "+hostip)
+  def get_domain_name(ip_address):
+    result=socket.gethostbyaddr(ip_address)
+    return list(result)[0]
+  # print("Domain name using PTR DNS:")
+  print("Reverse DNS address: "+get_domain_name(hostip))
+except socket.herror:
+  print("Could not find results for reverse DNS search!!!")
 print("\n-----Scanning Finished-----")
 rawpath = os.getcwd() + "\\SubdomainRoutes.py"
 path = rawpath.replace('\\', '/')
